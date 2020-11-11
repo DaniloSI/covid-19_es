413869fc7ea58fae3f454e16bbc7ae5fdd59a221
Danilo de Oliveira <danilodeoliveira94@gmail.com>
2020-11-11 13:51:38 -0300

    Atualização do Dashboard

--- dash/app.py
+++ dash/app.py
@@ -5,18 +5,104 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import os
+from datetime import datetime
 
 from components.graficos.Evolucao import figAreaAcumulados
 from components.graficos.ScatterMunicipio import figScatterCasosObitosAcumulado, figScatterIncidenciaLetalidade
 from components.mapas.ChoroplethIncidenciaLetalidade import rowChoropleph
 
+from components.data import data_ultima_notificacao
+from components.data import data_primeira_notificacao
+from components.data import total_casos_es
+from components.data import total_obitos_es
+from components.data import total_curas_es
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
+def format_date(date):
+    return datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
+
+last_date_text = '{} - {}'.format(format_date(data_primeira_notificacao), format_date(data_ultima_notificacao))
+
 # Renderiza componentes
 app.layout = dbc.Container(
     [
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                [
+                                    dbc.Card(
+                                        [
+                                            dbc.CardBody([
+                                                html.P('Intervalo de datas que compreendem os dados:', style={'textAlign': 'center'}),
+                                                html.H5(last_date_text, style={'textAlign': 'center'})
+                                            ])
+                                        ],
+                                        className="m-1"
+                                    ),
+                                ],
+                                className="pr-0"
+                            ),
+                            dbc.Col(
+                                [
+                                    dbc.Card(
+                                        [
+                                            dbc.CardBody([
+                                                html.P('Total de Casos Confirmados no Estado:', style={'textAlign': 'center'}),
+                                                html.H5(total_casos_es, style={'textAlign': 'center'})
+                                            ])
+                                        ],
+                                        className="m-1"
+                                    ),
+                                ],
+                                className="pl-0"
+                            )
+                        ]
+                    ),
+                    className="col-6 pr-0"
+                ),
+                dbc.Col(
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                [
+                                    dbc.Card(
+                                        [
+                                            dbc.CardBody([
+                                                html.P('Total de Óbitos no Estado:', style={'textAlign': 'center'}),
+                                                html.H5(total_obitos_es, style={'textAlign': 'center'})
+                                            ])
+                                        ],
+                                        className="m-1"
+                                    ),
+                                ],
+                                className="pr-0"
+                            ),
+                            dbc.Col(
+                                [
+                                    dbc.Card(
+                                        [
+                                            dbc.CardBody([
+                                                html.P('Total de Curados no Estado:', style={'textAlign': 'center'}),
+                                                html.H5(total_curas_es, style={'textAlign': 'center'})
+                                            ])
+                                        ],
+                                        className="m-1"
+                                    ),
+                                ],
+                                className="pl-0"
+                            )
+                        ]
+                    ),
+                    className="col-6 pl-0"
+                ),
+            ]
+        ),
         dbc.Row(
             [
                 dbc.Col(
--- dash/components/data.py
+++ dash/components/data.py
@@ -10,3 +10,9 @@ with open('../data/ES_MALHA_MUNICIPIOS.geojson') as json_file:
 df = pd.read_csv('../notebooks_output/microdados_pre-processed.csv', sep=',', encoding='UTF-8')
 
 df_municipios = pd.read_csv('../data/municipios.csv')
+
+data_ultima_notificacao = df['DataNotificacao'].max()
+data_primeira_notificacao = df['DataNotificacao'].min()
+total_casos_es = '{:,}'.format(int(df['Confirmados'].sum())).replace(',', '.')
+total_obitos_es = '{:,}'.format(int(df['Obitos'].sum())).replace(',', '.')
+total_curas_es = '{:,}'.format(int(df['Curas'].sum())).replace(',', '.')
